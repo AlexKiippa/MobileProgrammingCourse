076bb822254d81d0703af6d19b60496587cebf3d
Alex Kiippa <alex.kiippa@myy.haaga-helia.fi>
2023-08-24 15:38:55 +0300

    Laskin tehtävää täydennetty, printtaa laskutoimitukset nyt vain vierekkäin

--- mobileApp/components/laskin.js
+++ mobileApp/components/laskin.js
@@ -1,67 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import {FlatList, Image, Alert, Button, StyleSheet, Text, View, TextInput } from 'react-native';
-
-
+import { FlatList, Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function Laskin() {
-
-
-  const [text1, setText1] = useState("");
-  const [text2, setText2] = useState("");
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
   const [data, setData] = useState([]);
-  const [summa, setSumma] = useState("");
+  const [vastaus, setVastaus] = useState();
 
-  
 
   const buttonPressed1 = () => {
     let luku1 = parseInt(text1);
     let luku2 = parseInt(text2);
+    let summa = luku1 + luku2;
+    setVastaus(summa)
 
-    setSumma([luku1+luku2])
-    
-    setData([...data, { key: summa }]);
-     
-    };
+    setData([
+      ...data,
+      { key: luku1, type: 'number' },
+      { key: ' + ', type: 'operator' },
+      { key: luku2, type: 'number' },
+      { key: ' = ', type: 'operator' },
+      { key: summa.toString(), type: 'result' },
+      { key: '', type: 'empty' }, 
+    ]);
+  };
 
-    const buttonPressed2 = () => {
-      let luku1 = parseInt(text1);
-      let luku2 = parseInt(text2);
-  
-      setSumma([luku1-luku2])
-
-      setData([...data, { key: summa }]);
-     
-
-      };
-   
+  const buttonPressed2 = () => {
+    let luku1 = parseInt(text1);
+    let luku2 = parseInt(text2);
+    let summa = luku1 - luku2;
+    setVastaus(summa)
+    setData([
+      ...data,
+      { key: luku1, type: 'number' },
+      { key: ' - ', type: 'operator' },
+      { key: luku2, type: 'number' },
+      { key: ' = ', type: 'operator' },
+      { key: summa.toString(), type: 'result' },
+      { key: '', type: 'empty' }, 
+    ]);
+  };
 
   return (
     <View style={styles.container}>
-      
-      <Text>Result:{summa}</Text>   
-      <TextInput style={{width: 200, borderColor: 'gray', borderWidth: 1}}
-      keyboardType='numeric'
-onChangeText={text1 => setText1(text1)} value={text1} />
-
-<TextInput style={{width: 200, borderColor: 'gray', borderWidth: 1}}
-keyboardType='numeric'
-onChangeText={text2 => setText2(text2)} value={text2} />
-
-  <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-      <Button onPress={buttonPressed1} title="+" />
-      <Button onPress={buttonPressed2} title="-" />
-    
-    </View>
-    
+      <Text>Result:  {vastaus} </Text>
+      <TextInput
+        style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
+        keyboardType='numeric'
+        onChangeText={(text1) => setText1(text1)}
+        value={text1}
+      />
+      <TextInput
+        style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
+        keyboardType='numeric'
+        onChangeText={(text2) => setText2(text2)}
+        value={text2}
+      />
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+        <Button onPress={buttonPressed1} title='+' />
+        <Button onPress={buttonPressed2} title='-' />
+      </View>
       <Text>History</Text>
-
-
-      <FlatList  style={{ maxHeight: 200 }} data={data} renderItem={({item}) => <Text>{item.key}</Text>}
-	    keyExtractor={(item, index) => index.toString()} />
-     
-   
-      <StatusBar style="auto" />
+      <FlatList
+  style={{ maxHeight: 200 }}
+  data={data}
+  horizontal
+  renderItem={({ item }) => (
+    <View style={styles.itemContainer}>
+      <Text
+        style={[
+          item.type === 'result' ? styles.resultText : styles.normalText,
+          item.type === 'empty' && styles.emptyText, 
+        ]}
+      >
+        {item.key}
+      </Text>
+    </View>
+  )}
+  keyExtractor={(item, index) => index.toString()}
+/>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -72,5 +91,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  normalText: {
+    fontSize: 18,
+  },
+  emptyText: {
+    width: 30,
+  },
+  resultText: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
